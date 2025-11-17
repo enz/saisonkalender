58f76a70ad1cacb7708b3b1df1f971be0d7a1760
Markus Enzenberger <enz@users.sourceforge.net>
2025-11-17 09:47:12 +0100

    Clear old cache on activate event

--- sw.js
+++ sw.js
@@ -12,6 +12,18 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
